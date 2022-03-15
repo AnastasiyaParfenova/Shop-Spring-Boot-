fc5638efd56262a27dd303d95c94a4c0a116e785
Anastasiya <parfenova.anastasiya.98@yandex.ru>
2022-03-15 11:18:37 +0500

    Третий

--- src/main/java/com/example/shop/services/ProductService.java
+++ src/main/java/com/example/shop/services/ProductService.java
@@ -9,4 +9,19 @@ import java.util.List;
 @Service
 public class ProductService {
     private List<Product> products = new ArrayList<>();
+    private long ID = 0;
+    {
+        products.add(new Product(++ID, "PlayStation 5", "Simple description", 6700, "Ekaterinburg", "Tomas"));
+        products.add(new Product(++ID,"iPhone 11", "Simple description", 64700, "Moscow", "Tom"));
+    }
+    public List<Product> listProducts(){ return products; }
+
+    public void saveProduct(Product product) {
+        product.setId(++ID);
+        products.add(product);
+    }
+
+    public void deleteProduct(Long id) {
+        products.removeIf(product -> product.getId().equals(id));
+    }
 }
