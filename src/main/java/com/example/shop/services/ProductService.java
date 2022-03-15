a863f729658253b31705f832713e1af5d1dcc0b6
Anastasiya <parfenova.anastasiya.98@yandex.ru>
2022-03-15 11:04:06 +0500

    second

---
+++ src/main/java/com/example/shop/services/ProductService.java
@@ -0,0 +1,12 @@
+package com.example.shop.services;
+
+import com.example.shop.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private List<Product> products = new ArrayList<>();
+}
